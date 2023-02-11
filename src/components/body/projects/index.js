5cbdbe1e0c8b6f82cd620746c5a298881c483140
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-02-11 23:40:59 +0530

    projects

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import './project.css';
-import { ProjectData } from '../../data/project';
+import React from "react";
+import './projects.css';
+import { ProjectData } from "../../data/projects";
+import ProjectCard from "./project-card";
 
 function Projects() {
   const data = ProjectData;
   return (
-    <div className='projects'>
-      <label>Projects</label>     
+    <div className="projects">
+      <label className="section-title">Projects</label>  
       <div>
         {data.map((project) =>{
-          return
+          return <ProjectCard project={project}/>;
         })}
-      </div>
+        </div> 
+      
     </div>
   )
 }
